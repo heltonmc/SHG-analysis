2e082ff2467138e414a9f1cf7c668bd9946a1ecf
Michael Helton <57471570+heltonmc@users.noreply.github.com>
2020-06-16 17:14:23 -0400

    calculate sarcomere length

--- skeletal_analysis.jl
+++ skeletal_analysis.jl
@@ -43,23 +43,61 @@ function center_img(img)
     θ =  mode(round.(angles,digits =0))
     tfm = recenter(RotMatrix(deg2rad(θ)),[size(img)[1]/2,size(img)[1]/2])
     rot_img = warp(img, tfm)
+    return rot_img, θ
 end
 
 
 #get index values of local maximums of a column
-locmax = findlocalmaxima(img[:,col])
-idx = []
-for i in eachindex(locmax)
-    push!(idx,getindex(locmax,i)[1])
+function get_sarclength(rot_img)
+
+    sl_data = rot_img
+    for in_col in axes(rot_img)[1]
+
+        darray = rot_img[:,in_col]
+
+        locmax = findlocalmaxima(darray)
+        idx = []
+
+        for i in eachindex(locmax)
+            push!(idx,getindex(locmax,i)[1])
+        end
+
+        n = length(idx)
+
+        if n > 4
+
+            sarclength = zeros(n)
+            sarclength[1] = idx[2] - idx[1]
+            sarclength[n] = idx[n] - idx[n-1]
+            sarclength[2:n-1] = (idx[3:n] - idx[2:n-1] +idx[2:n-1] - idx[1:n-2])/2
+
+            spl = Spline1D(idx,sarclength)
+            sl = round.(spl(Vector(idx[1]:1:idx[end])),digits=0)
+
+            sl1 = darray
+            sl1[locmax[1][1]:locmax[end][1]] = sl
+        else
+            sl1 = darray
+        end
+
+
+        for i in eachindex(sl1)
+            if sl1[i] < 1.5
+                sl1[i] = 0
+            end
+        end
+
+    sl_data[:,in_col] = sl1
+    end
+
+return sl_data
 end
 
 
 
 
-    heatmap(abs.(img_processed))
+
+#    heatmap(abs.(img_processed))
 
     #save out file to read into MATLAB if necessary
-    writedlm("45%_15OCT19_Series031_location2_1xzoom_ch01.csv", img_processed, ',')
-
-
-#=
+#    writedlm("45%_15OCT19_Series031_location2_1xzoom_ch01.csv", img_processed, ',')
