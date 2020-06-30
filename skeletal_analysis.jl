8a079c0bff5f5b0f0cacb860ac00fc6390b696c9
Maeve Willen <mwillen@Maeves-MacBook-Air.local>
2020-06-30 12:04:28 -0400

    added functions damage_kernel, count_dam, perc_dam, and updated function center_img

--- skeletal_analysis.jl
+++ skeletal_analysis.jl
@@ -1,6 +1,6 @@
 #here is the OMI analysis file
 
-using FFTW, StatsBase, Plots, ImageView, Images, OffsetArrays, CoordinateTransformations, Dierckx
+using Distributions, FFTW, StatsBase, Plots, ImageView, Images, OffsetArrays, CoordinateTransformations, Dierckx, QuartzImageIO, Statistics
 
 ##### load image #####
 
@@ -39,11 +39,14 @@ function center_img(img)
     dimg = imfilter(img,centered(kernel))
     gy, gx = imgradients(dimg,KernelFactors.ando5)
     angles = rad2deg.(atan.(gx ./ gy))
+    d = fit_mle(Laplace, angles[:])
+    theta = d.μ
+    scale_factor = d.θ
 
-    θ =  mode(round.(angles,digits =0))
-    tfm = recenter(RotMatrix(deg2rad(θ)),[size(img)[1]/2,size(img)[1]/2])
+    tfm = recenter(RotMatrix(deg2rad(theta)),[size(img)[1]/2,size(img)[1]/2])
     rot_img = warp(img, tfm)
-    return rot_img, θ
+    return rot_img, theta
+
 end
 
 function recenter_img(img,θ)
@@ -111,6 +114,73 @@ function get_sarclength(rot_img)
 return sl_data
 end
 
+
+function damage_kernel(test)
+
+    freq_sl = mode(test)
+    std_sl = std(test)
+    weight = 1.1
+
+    sl_datarow = size(test)[1]
+    sl_datacol = size(test)[2]
+
+
+    kernel = 9;
+    kernel_mtrx = ones(kernel,kernel);
+    kernel_mtrx2 = -1 .* kernel_mtrx;
+
+
+    ctr_pxl = median(1:kernel) .|> Int64
+    nbr_pxl = (ctr_pxl - 1) .|> Int64
+
+
+    row_idx = range(ctr_pxl, step=kernel, stop=(sl_datarow-nbr_pxl))
+    column_idx = range(ctr_pxl, step=kernel, stop=(sl_datacol-nbr_pxl))
+
+
+    for col in values(column_idx)
+        for row in values(row_idx)
+
+            discrete_kernel = count(i->(i>freq_sl*weight), test[row-nbr_pxl:row+nbr_pxl, col-nbr_pxl:col+nbr_pxl]);
+
+            if discrete_kernel > floor(kernel/2)
+                test[row-nbr_pxl:row+nbr_pxl, col-nbr_pxl:col+nbr_pxl] = kernel_mtrx2;
+
+            end
+        end
+    end
+    return test
+end
+
+
+function count_dam(test, count_dmg, count_edge, count_norm)
+
+    for in_col in axes(test)[2]
+        test2 = test[:,in_col]
+
+        for i in eachindex(test2)
+            if test2[i] < 0.0
+                count_dmg = count_dmg + 1;
+            elseif test2[i] == 0.0
+                count_edge = count_edge + 1;
+            elseif test2[i] > 0.0
+                count_norm = count_norm + 1;
+            end
+        end
+    end
+    return count_dmg, count_edge, count_norm
+end
+
+
+function perc_dam(test, count_dmg, count_edge, count_norm)
+
+       total_grid = (size(test)[1] * size(test)[2]) - cnt_edge;
+       damage_perc = (cnt_dmg / total_grid) * 100
+
+       return damage_perc
+end
+
+
 #Sarcomere Length (μm)
 # this is a change
 function main_function(filename)
@@ -121,6 +191,13 @@ function main_function(filename)
     sl2 = recenter_img(sl,theta)
     replace!(sl2, NaN=> 0)
     sl2 = sl2[1:1024,1:1024]
+    dmg = damage_kernel(sl2)
+    count_dmg = 0;
+    count_norm = 0;
+    count_edge = 0;
+    cnt_dmg, cnt_edge, cnt_norm = count_dam(dmg, count_dmg, count_edge, count_norm)
+    damage_perc = perc_dam(dmg, cnt_dmg, cnt_edge, cnt_norm)
+
 end
 
 #heatmap(sl2,yflip=true)
